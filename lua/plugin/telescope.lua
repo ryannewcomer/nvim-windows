bd5f325afbefbebbb65731d1a24e91592c51c8da
Ryan Hung <ryanhungryan@gmail.com>
2024-05-13 18:04:01 +0100

    config telescope

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,3 +1,29 @@
 return {
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.6',
+	dependencies = 'nvim-lua/plenary.nvim',
+	keys = {
+		{
+				";f",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.find_files({
+						no_ignore = false,
+						hidden = true,
+					})
+				end,
+				desc = "Lists files in your current working directory, respects .gitignore",
+			},
+			{
+				";r",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.live_grep({
+						additional_args = { "--hidden" },
+					})
+				end,
+				desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
+			},
+	}
 	require('telescope').setup{}
 }
