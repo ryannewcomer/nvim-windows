dcf0ee2cb8f5edb444e9fa4ad9feac06cb7ff12c
Ryan Hung <ryanhungryan@gmail.com>
2024-05-15 13:08:47 +0100

    feat(treesitter.lua): install treesitter and config it
    install nvim-treesitter and config it to install lua,python and golang style

---
+++ lua/plugin/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+	'nvim-treesitter/nvim-treesitter',
+	build = ":TSUpdate",
+	config = function()
+		local configs = require('nvim-treesitter.configs')
+
+		configs.setup({
+			ensure_install = {"lua","python","golang"},
+			sync_install = false,
+			highlight = { enable = true},
+			indent = { enable = true},
+		})
+	end
+}
+
