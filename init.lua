4af889a5d35f737cf2649d1c64f8927e661c45b9
Ryan Hung <ryanhungryan@gmail.com>
2024-05-16 11:02:13 +0100

    fix(init.lua): move all option and mapping to init.lua

--- init.lua
+++ init.lua
@@ -10,8 +10,69 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
 -- set mapleader
 vim.g.mapleader = " "
+
 require("lazy").setup("plugin")
+
+
+--option
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+vim.cmd.colorscheme("oldworld")
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
+
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_newtrwPlugin = 1
+
+-- enable 24-bit color
+vim.opt.termguicolors = true
+
+-- clipboard
+vim.api.nvim_command('set clipboard=unnamedplus')
+
+-- mapping
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true}
+
+-- Increment/decrement
+keymap.set("n", "<C-+>", "<C-a>")
+keymap.set("n", "<C-->", "<C-x>")
+
+--select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+
+--tab
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+
 
